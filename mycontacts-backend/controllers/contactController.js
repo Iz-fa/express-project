297022cad1e344c91aad2fa95c6fdebb783a7379
Iz-fa <fastol35@gmail.com>
2026-02-04 12:56:51 +0100

    Setting a file of constants for errors + using switch statement for all kinds of errors+ using express-async-handler

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,30 +1,31 @@
 // status 200 means request succeded, 201 means created, 204 is when theres no response body
 // status 400 Bad request Invalid input
 
+// This handles exceptions and give them back to errorHandler.js so no need to use throw catch
+const asyncHandler = require("express-async-handler");
+
 // @desc Get all contacts
 // @route GET /api/contacts
 // @access public
-const getContacts = (req,res)=>{
+const getContacts = asyncHandler(async(req,res)=>{  //async because mongoDB uses promises
     res.status(200).json({
         message: "Get all contacts"
     });
-};
+});
 
 // @desc Get contact
 // @route GET /api/contacts/:id
 // @access public
-const getContact = (req, res)=>{
+const getContact = asyncHandler(async(req, res)=>{
     res.status(200).json({
         message: `Get Contact for ${req.params.id}`
     });
-};
+});
 
 // @desc Create new contact
 // @route POST /api/contacts
 // @access public
-const createContact = (req, res)=>{
-    
-    console.log(req.body);
+const createContact = asyncHandler(async(req, res)=>{
     const {name, email, phone} = req.body;
     if(!name || !email || !phone){
         res.status(400);
@@ -34,25 +35,25 @@ const createContact = (req, res)=>{
     res.status(201).json({       
         message: "Create Contact"
     });
-};
+});
 
 // @desc Update contact
 // @route PUT /api/contacts/:id
 // @access public
-const updateContact = (req, res)=>{
+const updateContact = asyncHandler(async(req, res)=>{
     res.status(200).json({
         message: `Update contact for ${req.params.id}`
     });
-};
+});
 
 // @desc Delete contact
 // @route DELETE /api/contacts/:id
 // @access public
-const deleteContact = (req, res)=>{
+const deleteContact = asyncHandler(async(req, res)=>{
     res.status(200).json({
         message: `Delete contact for ${req.params.id}`
     });
-};
+});
 
 
 
--- mycontacts-backend/middleware/errorHandler.js
+++ mycontacts-backend/middleware/errorHandler.js
@@ -1,6 +1,36 @@
+const {errors} = require("../errors");
+
 const errorHandler = (err,req,res,next)=>{
     const statusCode = res.statusCode? res.statusCode: 500;
-    res.json({message: err.message, stackTrace: err.stack});
+    console.log(statusCode);
+    console.log(errors.VALIDATION_FAILED);
+
+    switch(statusCode){
+        case errors.VALIDATION_FAILED:
+            res.json({title: "Validation Failed", message: err.message, stackTrace: err.stack});
+            break;
+
+        case errors.UNAUTHORIZED:
+            res.json({title: "Unauthorized", message: err.message, stackTrace: err.stack  });
+            break;
+
+        case errors.FORBIDDEN:
+            res.json({title:"Forbidden", message: err.message, stackTrace: err.stack});
+            break;
+
+        case errors.NOT_FOUND:
+            res.json({title: "Not Found", message: err.message, stackTrace: err.stack});
+            break;
+
+        case errors.SERVER_ERROR:
+            res.json({title: "Server Error", message: err.message, stackTrace: err.stack});
+            break;
+
+        default:
+            res.send("No Errors, we're good!");
+            break;
+
+    } 
 };
 
 module.exports = errorHandler;
