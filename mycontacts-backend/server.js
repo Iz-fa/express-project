c2dcfbcb08abe9facb92aa8afd424ad7d58b7c21
Iz-fa <fastol35@gmail.com>
2026-02-04 01:23:07 +0100

    Setting up the router with get post put delete and testing with thunder client extension

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -5,6 +5,15 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+/*
+app.get('/api/contacts', (req,res)=>{
+    res.status(200).json({
+        message: "Get all contacts"
+    });
+});
+*/
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
