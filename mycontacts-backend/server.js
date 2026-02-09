f8927562821b2e59a8b072522edd59c59e68490b
Iz-fa <fastol35@gmail.com>
2026-02-09 11:39:41 +0100

    Setting up the route file for User

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const connectDb = require("./config/dbConnection")
 const errorHandler = require("./middleware/errorHandler");
-const router = require("./routes/contactRoutes");
+const contactRouter = require("./routes/contactRoutes");
+const userRouter = require("./routes/userRoutes");
 const dotenv = require("dotenv").config();
 
 
@@ -22,7 +23,8 @@ app.get('/api/contacts', (req,res)=>{
 //this middleware from express is to parse the json data the client is sending back to the server 
 // (because the data is a string)
 app.use(express.json()); 
-app.use("/api/contacts", router);
+app.use("/api/contacts", contactRouter);
+app.use("api/users", userRouter);
 
 // since errorHandler funciton has 4 params, this tells express
 // that this middleware is to handle the error that was thrown
