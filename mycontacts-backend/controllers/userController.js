b2fc81a213b3ed277e7a75f4fd2ee5d5a1f89374
Iz-fa <fastol35@gmail.com>
2026-02-21 13:52:04 +0100

    Adjusting the registerUser function in the controller

--- mycontacts-backend/controllers/userController.js
+++ mycontacts-backend/controllers/userController.js
@@ -1,10 +1,29 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 // @desc Register a user
 // @route POST /api/users/register
 // @access public
-const registerUser = asyncHandler(async(req,res)=>{        
-   res.json({message:"Register the user"});
+const registerUser = asyncHandler(async(req,res)=>{
+
+
+   const {username, email, password} = req.body;
+   if(!username || !email|| !password){
+      res.status(400);
+      throw new Error("All fields are mandatory!");
+   }
+   const userExists = await User.findOne({email});
+   if(userExists){
+      res.status(400);
+      throw new Error("This Email is already registered");
+   }
+
+   const user = await User.create({
+      username: username,
+      email: email,
+      password: password
+   });       
+   res.status(201).json(user);
 });
 
 // @desc Login user
